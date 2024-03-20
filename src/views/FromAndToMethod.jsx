2a0802c7543eed620beb54738fb91d27ab0a535a
Joseph Wilde <jwilde19@gmail.com>
2024-03-19 21:54:38 -0400

    Create file

---
+++ src/views/FromAndToMethod.jsx
@@ -0,0 +1,14 @@
+// Imported GSAP utilities
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+const FromAndToMethod = () => {
+  return (
+    <div className='flex justify-center'>
+      <h1 className=''>Boring</h1>
+      <div></div>
+    </div>
+  )
+}
+
+export default FromAndToMethod

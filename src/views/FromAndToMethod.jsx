e7787379ae105d2773ee9848c5b32e6a7a85afa3
Joseph Wilde <jwilde19@gmail.com>
2024-03-20 21:34:41 -0400

    Start GSAP

--- src/views/FromAndToMethod.jsx
+++ src/views/FromAndToMethod.jsx
@@ -1,12 +1,32 @@
+import { useRef } from 'react'
+
 // Imported GSAP utilities
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import React from 'react'
 
 const FromAndToMethod = () => {
+  const h1Ref = useRef()
+  const progressRef = useRef()
+
+  useGSAP(() => {
+    gsap.to(progressRef.current, {
+      width: '100%',
+    })
+  })
+
   return (
-    <div className='flex justify-center'>
-      <h1 className=''>Boring</h1>
-      <div></div>
+    <div className='flex justify-center items-center h-vh bg-dark'>
+      <h1
+        ref={h1Ref}
+        className='text-[30rem] text-center opacity-[0.3] uppercase font-bebas text-gray-600'
+      >
+        Boring
+      </h1>
+      <div
+        ref={progressRef}
+        className='bg-[#A1BCFF] h-[3px] w-0 absolute top-[50%]'
+      ></div>
     </div>
   )
 }

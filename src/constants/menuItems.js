f45f6867a6d330fcf129ce6d1d5de056ff9bc499
Joseph Wilde <jwilde19@gmail.com>
2024-03-21 17:05:03 -0400

    Create file

---
+++ src/constants/menuItems.js
@@ -0,0 +1,28 @@
+const menuItems = [
+  {
+    name: 'Home',
+    route: '/',
+  },
+  {
+    name: 'GreenSock',
+    route: '/greensock',
+  },
+  {
+    name: 'Scroll Animations',
+    route: '/scrollanimations',
+  },
+  {
+    name: 'Text Effects',
+    route: '/texteffects',
+  },
+  {
+    name: 'Horizontal Scrolling',
+    route: '/horizontalscrolling',
+  },
+  {
+    name: 'Parallax',
+    route: '/parallax',
+  },
+]
+
+export default menuItems
